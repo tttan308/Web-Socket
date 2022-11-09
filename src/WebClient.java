54e7a9f38fe4723d00a9d5464f40642fed75f5ee
tttan308 <tantran.300803@gmail.com>
2022-11-09 15:10:26 +0700

    update download file pdf, txt,...

--- src/WebClient.java
+++ src/WebClient.java
@@ -1,14 +1,8 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.io.FileWriter;
+import java.io.*;
+import java.net.*;
 
 public class WebClient {
-
+    
     public static void main(String[] args) throws IOException {
 
         // if( args.length != 1)                                                      
@@ -17,36 +11,45 @@ public class WebClient {
         //     System.exit( 0 );
         // }
         // String url = args[0];
-        String url = "example.com";
-        String path = "/index.html";
+        String url = "anglesharp.azurewebsites.net";
+        String path = "/Chunked";
         try {
             Socket socket = new Socket(url, 80); 
-            PrintStream output = new PrintStream(socket.getOutputStream());
-            output.print("GET " + path + " HTTP/1.1\r\n");
-            output.print("HOST: "+ url + "\r\n");
-            output.print("\r\n");
-            output.flush();
-            socket.shutdownOutput();
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream req = new PrintStream(socket.getOutputStream());
 
-            // if(path == "/"){
+            req.print("GET " + path + " HTTP/1.1\r\n");
+            req.print("HOST: "+ url + "\r\n");
+            req.print("\r\n");
+            req.flush();
+            socket.shutdownOutput();
+            // BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataInputStream recv = new DataInputStream(socket.getInputStream());
+            if(path == "/Chunked"){
                 File file = new File("./binary/" + url + "_index.html");
                 boolean createFile = file.createNewFile();
                 System.out.println("File created: "+ file.getName());
-                FileWriter fw = new FileWriter(file);
-                String outStr;
-                while ((outStr = input.readLine()) != null) {
-                    fw.write(outStr + "\n");
+                FileOutputStream fw = new FileOutputStream(file);
+                byte[] outStr = new byte[1024];
+                int len;
+                while((len = recv.read(outStr)) != -1){
+                    fw.write(outStr, 0, len);
                 }
                 fw.close();
-            // }
-            // else{
-            //     File fileName = new File("./binary/" + url + path);
-            //     FileOutputStream newFile = new FileOutputStream(fileName);
-            // }
-            input.close();
-            output.close();
-            socket.close();              
+            }
+            else if(path.contains(".txt")){
+                File file = new File("./binary/" + url + ".txt");
+                boolean createFile = file.createNewFile();
+                System.out.println("File created: "+ file.getName());
+                FileOutputStream fw = new FileOutputStream(file);
+                byte[] outStr = new byte[1024];
+                int len;
+                while((len = recv.read(outStr)) != -1){
+                    
+                    fw.write(outStr, 0, len);
+                }
+                fw.close();
+            }
+            
         } 
         catch (IOException e) {
             System.out.println("Error: " + e);
