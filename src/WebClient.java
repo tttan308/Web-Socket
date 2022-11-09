0d11c6db1c9f80be10fbb84897d60d08411136f3
tttan308 <tantran.300803@gmail.com>
2022-11-09 15:48:53 +0700

    download file done!

--- src/WebClient.java
+++ src/WebClient.java
@@ -4,52 +4,48 @@ import java.net.*;
 public class WebClient {
     
     public static void main(String[] args) throws IOException {
-
-        // if( args.length != 1)                                                      
-        // {
-        //     System.out.println( "Usage: SimpleSocketClientExample <server> <path>" );
-        //     System.exit( 0 );
-        // }
-        // String url = args[0];
-        String url = "anglesharp.azurewebsites.net";
-        String path = "/Chunked";
+        String url = "web.stanford.edu";
+        String path = "/dept/its/support/techtraining/techbriefing-media/Intro_Net_91407.ppt";
         try {
+            //Create socket
             Socket socket = new Socket(url, 80); 
             PrintStream req = new PrintStream(socket.getOutputStream());
+            DataInputStream recv = new DataInputStream(socket.getInputStream());
 
+            //Send request to server with url and path
             req.print("GET " + path + " HTTP/1.1\r\n");
             req.print("HOST: "+ url + "\r\n");
             req.print("\r\n");
             req.flush();
             socket.shutdownOutput();
-            // BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            DataInputStream recv = new DataInputStream(socket.getInputStream());
-            if(path == "/Chunked"){
-                File file = new File("./binary/" + url + "_index.html");
-                boolean createFile = file.createNewFile();
-                System.out.println("File created: "+ file.getName());
-                FileOutputStream fw = new FileOutputStream(file);
-                byte[] outStr = new byte[1024];
-                int len;
-                while((len = recv.read(outStr)) != -1){
-                    fw.write(outStr, 0, len);
+
+            //Handle 
+            File file;
+            if(path.substring(path.length()-1) != "/" || path.length() == 1)
+            {
+                //File Handling
+                if(path == "/") file = new File("./binary/" + url + "_index.html");
+                else file = new File("./binary/" + url + "_" + path.substring(path.lastIndexOf("/") + 1, path.length()));
+                if(file.createNewFile() == false)
+                {
+                    file.delete();
+                    file.createNewFile();
                 }
-                fw.close();
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] buffer = new byte[1024];
+                int len;
+                while((len = recv.read(buffer)) != -1)
+                {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
             }
-            else if(path.contains(".txt")){
-                File file = new File("./binary/" + url + ".txt");
-                boolean createFile = file.createNewFile();
-                System.out.println("File created: "+ file.getName());
-                FileOutputStream fw = new FileOutputStream(file);
-                byte[] outStr = new byte[1024];
-                int len;
-                while((len = recv.read(outStr)) != -1){
-                    
-                    fw.write(outStr, 0, len);
-                }
-                fw.close();
+            else{
+                //Folder Handling
+
             }
             
+            socket.close();
         } 
         catch (IOException e) {
             System.out.println("Error: " + e);
