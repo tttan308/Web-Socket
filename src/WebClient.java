96bb755d2172685c7f51d0a24a6839f333d6be13
SnowyField1906 <aswdqe1x@gmail.com>
2022-11-12 03:50:53 +0700

    add folder handler & optimize

--- src/WebClient.java
+++ src/WebClient.java
@@ -1,26 +1,48 @@
 import java.io.*;
 import java.net.*;
 
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.stream.Collectors;
+// import java.util.stream.Stream;
+
 // import java.nio.charset.StandardCharsets;
 
 public class WebClient {
-  static String url = "web.stanford.edu/class/cs224w/slides/01-intro.pdf";
+  static String url =
+    "http://www-net.cs.umass.edu/wireshark-labs/Wireshark_Intro_v8.1.docx";
 
-  static String host = url.substring(0, url.indexOf("/"));
-  static String get = url.substring(url.indexOf("/"));
-  static String path = url.substring(url.lastIndexOf("/") + 1);
+  //====== MAIN ======//
 
   public static void main(String[] args)
     throws IOException, InterruptedException {
-    if (path.equals("")) path = "index.html";
+    url = url.replace("http://", "");
+
+    System.out.println(host());
+    System.out.println(get());
+    System.out.println(path());
 
     try {
-      //Create socket
-      Socket socket = new Socket(host, 80);
+      // Create socket
+      Socket socket = new Socket(host(), 80);
 
-      String name = "./binary/" + host + "_" + path;
+      File file = new File(path());
 
-      Download(socket, name);
+      if (isSubfolder()) {
+        file.mkdirs();
+        // Path path = Paths.get(url);
+        // List<Path> paths;
+        // try (Stream<Path> walk = Files.walk(path)) {
+        //   paths =
+        //     walk.filter(Files::isRegularFile).collect(Collectors.toList());
+        // }
+        // paths.forEach(x -> System.out.println(x));
+      } else {
+        Download(socket, file);
+      }
 
       System.out.println("Success!");
 
@@ -30,32 +52,60 @@ public class WebClient {
     }
   }
 
-  private static void Download(Socket s, String name) throws IOException {
-    File file = new File(name);
+  //====== CALL METHODS ======//
 
+  private static boolean isRoot() {
+    return url.split("/").length == 1;
+  }
+
+  private static boolean isSubfolder() {
+    return !isRoot() && !url.substring(url.lastIndexOf("/") + 1).contains(".");
+  }
+
+  private static String host() {
+    return url.split("/")[0];
+  }
+
+  private static String get() {
+    if (isRoot()) return "index.html";
+    String get = url.substring(url.indexOf("/", 8));
+    if (isSubfolder()) return get + "*";
+    return get;
+  }
+
+  private static String path() {
+    if (isSubfolder()) return (
+      "./" + url.split("/")[url.split("/").length - 1] + "/"
+    );
+
+    String path = "./binary/" + host() + "_";
+    if (isRoot()) return path + "index.html";
+    return path + url.substring(url.lastIndexOf("/") + 1);
+  }
+
+  //====== SEND METHODS ======//
+
+  private static void Download(Socket s, File file) throws IOException {
+    // Initialize streams
     PrintStream ps = new PrintStream(s.getOutputStream());
     BufferedInputStream bis = new BufferedInputStream(s.getInputStream());
     FileOutputStream fos = new FileOutputStream(file);
 
-    //Send request to server
-    ps.print("GET " + get + " HTTP/1.1\r\n");
-    ps.print("HOST: " + host + "\r\n");
+    // Send request to server
+    ps.print("GET " + get() + " HTTP/1.1\r\n");
+    ps.print("HOST: " + host() + "\r\n");
     ps.print("\r\n");
     ps.flush();
     s.shutdownOutput();
 
-    if (!file.exists()) {
-      file.delete();
-      file.createNewFile();
-    }
-
+    // Write to file
     byte[] buffer = new byte[1024];
     int len;
-
     while ((len = bis.read(buffer)) != -1) {
       fos.write(buffer, 0, len);
     }
 
+    // Close streams
     fos.close();
     bis.close();
     ps.close();
