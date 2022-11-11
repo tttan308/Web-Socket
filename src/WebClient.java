b976959335c03a9f58e6f7135bef2e2177ecf00e
tttan308 <tantran.300803@gmail.com>
2022-11-11 22:14:13 +0700

    update new version

--- src/WebClient.java
+++ src/WebClient.java
@@ -2,89 +2,51 @@ import java.io.*;
 import java.net.*;
 
 public class WebClient {
-  static String url;
-  static String path;
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "gaia.cs.umass.edu";
+        String path = "/wireshark-labs/alice.txt";
+        try {
+            //Create socket
+            Socket socket = new Socket(url, 80); 
+            PrintStream req = new PrintStream(socket.getOutputStream());
+            DataInputStream recv = new DataInputStream(socket.getInputStream());
 
-  public static File getFile() throws IOException {
-    URL u = new URL(url);
-    URLConnection uc = u.openConnection();
-    int contentLength = uc.getContentLength();
-
-    try (InputStream raw = uc.getInputStream()) {
-      InputStream in = new BufferedInputStream(raw);
-      byte[] data = new byte[contentLength];
-      int bytesRead = 0;
-      int offset = 0;
-      while (offset < contentLength) {
-        bytesRead = in.read(data, offset, data.length - offset);
-        if (bytesRead == -1) break;
-        offset += bytesRead;
-      }
-
-      if (offset != contentLength) {
-        throw new IOException(
-          "Only read " + offset + " bytes; Expected " + contentLength + " bytes"
-        );
-      }
-
-      String filename = u.getFile();
-      filename = filename.substring(filename.lastIndexOf('/') + 1);
-      File f = new File(filename);
-      try (FileOutputStream fout = new FileOutputStream(f)) {
-        fout.write(data);
-        fout.flush();
-      }
-      return f;
+            //Send request to server with url and path
+            req.print("GET " + path + " HTTP/1.1\r\n");
+            req.print("HOST: "+ url + "\r\n");
+            req.print("\r\n");
+            req.flush();
+            socket.shutdownOutput(); 
+            String name = (path == "/")
+            ? "index.html"
+            : path.substring(path.lastIndexOf("/") + 1);
+    
+          String fullPathName = "./binary/" + url + "_" + name;
+    
+          File file = new File(fullPathName);
+    
+          if (!file.exists()) {
+            file.delete();
+            file.createNewFile();
+          }
+    
+          FileOutputStream fos = new FileOutputStream(file);
+    
+          byte[] buffer = new byte[1024];
+          int len;
+          while ((len = recv.read(buffer)) != -1) {
+            fos.write(buffer, 0, len);
+          }
+    
+          fos.close();
+            System.out.println("Success!");
+            recv.close();
+            req.close();
+            socket.close();
+        } 
+        catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
-  }
-
-  public static void main(String[] args) throws IOException {
-    url = "www-net.cs.umass.edu";
-    path = "/wireshark-labs/Wireshark_Intro_v8.1.docx";
-
-    try {
-      Socket socket = new Socket(url, 80);
-      PrintStream req = new PrintStream(socket.getOutputStream());
-      DataInputStream recv = new DataInputStream(socket.getInputStream());
-
-      req.print("GET " + path + " HTTP/1.1\r\n");
-      req.print("HOST: " + url + "\r\n");
-      req.print("\r\n");
-      req.flush();
-      socket.shutdownOutput();
-
-      // if (path.substring(path.length() - 1) != "/" || path.length() == 1) {
-
-      // String name = (path == "/")
-      //   ? "index.html"
-      //   : path.substring(path.lastIndexOf("/") + 1);
-
-      // String fullPathName = "./binary/" + url + "_" + name;
-
-      // File file = new File(fullPathName);
-
-      // if (!file.exists()) {
-      //   file.delete();
-      //   file.createNewFile();
-      // }
-
-      FileOutputStream fos = new FileOutputStream(getFile());
-
-      byte[] buffer = new byte[1024];
-      int len;
-      while ((len = recv.read(buffer)) != -1) {
-        fos.write(buffer, 0, len);
-      }
-
-      fos.close();
-
-      // } else {}
-
-      System.out.println("Success!");
-
-      socket.close();
-    } catch (IOException e) {
-      System.out.println("Error: " + e);
-    }
-  }
 }
