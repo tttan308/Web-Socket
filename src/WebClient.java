5c9d997af7fcd2d2037aac1e881b47bcab1194ed
SnowyField1906 <aswdqe1x@gmail.com>
2022-11-12 01:00:37 +0700

    fix get

--- src/WebClient.java
+++ src/WebClient.java
@@ -1,58 +1,63 @@
 import java.io.*;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
+
+// import java.nio.charset.StandardCharsets;
 
 public class WebClient {
+  static String url = "web.stanford.edu/class/cs224w/slides/01-intro.pdf";
+
+  static String host = url.substring(0, url.indexOf("/"));
+  static String get = url.substring(url.indexOf("/"));
+  static String path = url.substring(url.lastIndexOf("/") + 1);
 
   public static void main(String[] args)
     throws IOException, InterruptedException {
-    String url = "example.com/";
-
-    String host = url.substring(0, url.indexOf("/"));
-    String path = url.substring(url.indexOf("/"));
-    if (path.equals("/")) path = "index.html";
+    if (path.equals("")) path = "index.html";
 
     try {
       //Create socket
       Socket socket = new Socket(host, 80);
-      PrintStream req = new PrintStream(socket.getOutputStream());
-      BufferedInputStream recv = new BufferedInputStream(
-        socket.getInputStream()
-      );
 
-      //Send request to server with url and path
-      req.print("GET " + path + " HTTP/1.1\r\n");
-      req.print("HOST: " + host + "\r\n");
-      req.print("\r\n");
-      req.flush();
-      socket.shutdownOutput();
+      String name = "./binary/" + host + "_" + path;
 
-      String fullPathName = "./binary/" + host + "_" + path;
+      Download(socket, name);
 
-      File file = new File(fullPathName);
-
-      if (!file.exists()) {
-        file.delete();
-        file.createNewFile();
-      }
-
-      FileOutputStream fos = new FileOutputStream(file);
-
-      byte[] buffer = new byte[1024];
-      int len;
-
-      // Get body
-      while ((len = recv.read(buffer)) > 0) {
-        fos.write(buffer, 0, len);
-      }
-
-      fos.close();
       System.out.println("Success!");
-      recv.close();
-      req.close();
+
       socket.close();
     } catch (IOException e) {
       System.out.println("Error: " + e);
     }
+  }
+
+  private static void Download(Socket s, String name) throws IOException {
+    File file = new File(name);
+
+    PrintStream ps = new PrintStream(s.getOutputStream());
+    BufferedInputStream bis = new BufferedInputStream(s.getInputStream());
+    FileOutputStream fos = new FileOutputStream(file);
+
+    //Send request to server
+    ps.print("GET " + get + " HTTP/1.1\r\n");
+    ps.print("HOST: " + host + "\r\n");
+    ps.print("\r\n");
+    ps.flush();
+    s.shutdownOutput();
+
+    if (!file.exists()) {
+      file.delete();
+      file.createNewFile();
+    }
+
+    byte[] buffer = new byte[1024];
+    int len;
+
+    while ((len = bis.read(buffer)) != -1) {
+      fos.write(buffer, 0, len);
+    }
+
+    fos.close();
+    bis.close();
+    ps.close();
   }
 }
