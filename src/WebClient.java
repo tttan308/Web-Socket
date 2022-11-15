a74523a1bedfb56cf2aec9bf80b825116edf4c60
SnowyField <aswdqe1x@gmail.com>
2022-11-16 02:36:50 +0700

    add subfolder request handler

--- src/WebClient.java
+++ src/WebClient.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class WebClient {
-  static String url ="http://web.stanford.edu/dept/its/support/techtraining/techbriefing-media/Intro_Net_91407.ppt";
-
-  // static String url = "example.com";
+  static String url = "http://web.stanford.edu/class/cs231a/course_notes/";
 
   //====== MAIN ======//
 
@@ -14,23 +12,59 @@ public class WebClient {
 
     System.out.println("Host: " + host());
     System.out.println("Get: " + get());
-    System.out.println("Path: " + path());
 
     try {
-      // Create socket
-      Socket socket = new Socket(host(), 80);
-
-      File file = new File(path());
+      // Create socket and streams
+      Socket s = new Socket(host(), 80);
+      PrintStream ps = new PrintStream(s.getOutputStream());
+      InputStream is = s.getInputStream();
 
       if (isSubfolder()) {
-        file.mkdirs();
+        File folder = new File("./" + host() + "_" + fileName());
+        folder.mkdirs();
+        ps = new PrintStream(s.getOutputStream());
+
+        // Send request to server
+
+        ps.print("GET " + get() + " HTTP/1.1\r\n");
+        ps.print("Host: " + host() + "\r\n");
+        ps.print("\r\n");
+        ps.flush();
+
+        // Read response from server
+        BufferedReader br = new BufferedReader(
+          new InputStreamReader(s.getInputStream())
+        );
+        String line = br.readLine();
+
+        while (line != null) {
+          if (line.contains("href=\"")) {
+            int firstIndex = line.indexOf("href=\"") + 6;
+            int lastIndex = line.indexOf("\"", firstIndex);
+            String fileName = line.substring(firstIndex, lastIndex);
+
+            if (!fileName.contains("http://") && fileName.contains(".")) {
+              try {
+                System.out.println("Downloading " + fileName);
+                Download(s, is, ps, fileName);
+              } catch (Exception e) {
+                System.out.println("Error downloading " + fileName);
+              }
+            }
+          }
+          line = br.readLine();
+        }
+
+        br.close();
       } else {
-        Download(socket, file);
+        Download(s, is, ps, fileName());
       }
 
       System.out.println("Success!");
 
-      socket.close();
+      // Close streams and socket
+      ps.close();
+      s.close();
     } catch (IOException e) {
       System.out.println("Error: " + e);
     }
@@ -51,62 +85,64 @@ public class WebClient {
   }
 
   private static String get() {
-    if (isRoot()) return "index.html";
-    String get = url.substring(url.indexOf("/", 8));
-    if (isSubfolder()) return get + "*";
-    return get;
+    if (isRoot()) return "/";
+    return url.substring(url.indexOf("/", 8));
   }
 
-  private static String path() {
-    if (isSubfolder()) return (
-      "./" + url.split("/")[url.split("/").length - 1] + "/"
-    );
+  private static String path(String fileName) {
+    if (isSubfolder()) return "./" + host() + "_" + fileName() + "/" + fileName;
+    return "./binary/" + host() + "_" + fileName;
+  }
 
-    String path = "./binary/" + host() + "_";
-    if (isRoot()) return path + "index.html";
-    return path + url.substring(url.lastIndexOf("/") + 1);
+  private static String fileName() {
+    if (isRoot()) return "index.html";
+    return url.split("/")[url.split("/").length - 1];
   }
 
   //====== SEND METHODS ======//
 
-  private static void Download(Socket s, File file) throws IOException {
-    // Initialize streams
-    PrintStream ps = new PrintStream(s.getOutputStream());
-    InputStream bis = s.getInputStream();
+  private static void Download(
+    Socket s,
+    InputStream is,
+    PrintStream ps,
+    String fileName
+  )
+    throws IOException {
+    // Initialize file
+    File file = new File(path(fileName));
+
+    // Initialize stream
     FileOutputStream fos = new FileOutputStream(file);
 
     // Send request to server
-    ps.print("GET " + get() + " HTTP/1.1\r\n");
+    ps.print("GET " + get() + fileName + " HTTP/1.1\r\n");
     ps.print("Host: " + host() + "\r\n");
     ps.print("\r\n");
     ps.flush();
-    s.shutdownOutput();
+    // s.shutdownOutput();
 
     // Read response from server
     int count, offset;
     byte[] buffer = new byte[2048];
     boolean eohFound = false;
-    while ((count = bis.read(buffer)) != -1)
-    {
-        offset = 0;
-        if(!eohFound){
-            String string = new String(buffer, 0, count);
-            int indexOfEOH = string.indexOf("\r\n\r\n");
-            if(indexOfEOH != -1) {
-                count = count-indexOfEOH-4;
-                offset = indexOfEOH+4;
-                eohFound = true;
-            } else {
-                count = 0;
-            }
+    while ((count = is.read(buffer)) != -1) {
+      offset = 0;
+      if (!eohFound) {
+        String string = new String(buffer, 0, count);
+        int indexOfEOH = string.indexOf("\r\n\r\n");
+        if (indexOfEOH != -1) {
+          count = count - indexOfEOH - 4;
+          offset = indexOfEOH + 4;
+          eohFound = true;
+        } else {
+          count = 0;
         }
+      }
       fos.write(buffer, offset, count);
       fos.flush();
     }
 
-    // Close streams
+    is.close();
     fos.close();
-    bis.close();
-    ps.close();
   }
 }
