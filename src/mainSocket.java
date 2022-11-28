812c7d9d75c1dfb2cc225e08a385dd46267b873a
tttan308 <tantran.300803@gmail.com>
2022-11-28 13:36:28 +0700

    update

--- src/mainSocket.java
+++ src/mainSocket.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 
 public class mainSocket {
     public static void main(String[] args) throws InterruptedException, IOException{
         String[] urls = {"http://web.stanford.edu/class/cs224w/slides/08-GNN-application.pdf","http://web.stanford.edu/class/cs231a/project.html"};
-        Runnable r1 = new WebSocket(urls[0]);
-        Thread t1 = new Thread(r1);
-        Runnable r2 = new WebSocket(urls[1]);
-        Thread t2 = new Thread(r2);
-
-        t1.start();
-        t2.start();
+        
+        ExecutorService executor = java.util.concurrent.Executors.newFixedThreadPool(2);
+        executor.execute(new WebSocket(urls[0]));
+        executor.execute(new WebSocket(urls[1]));
+        executor.shutdown();
     }
 }
